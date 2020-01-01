ce13cbcb80eb3e4f27c1d979a9f49ed862d6cef1
FJ <francois.jiang@centrale-marseille.fr>
2020-01-01 12:16:49 +0100

    Déplacements

--- taquin.js
+++ taquin.js
@@ -13,6 +13,7 @@ function melange() {
     document.getElementById("i6").src = "Images/6.png";
     document.getElementById("i7").src = "Images/7.png";
     document.getElementById("vide").src = "Images/vide.png";
+    move = true;
 }
 
 melange();
@@ -32,6 +33,36 @@ function victoire() {
     document.getElementById("i6").src = "Images/v6.png";
     document.getElementById("i7").src = "Images/v7.png";
     document.getElementById("vide").src = "Images/v8.png";
-    alert('Victoire');
+    alert('Victoire !!!');
     return 0;
+}
+
+function indice(k) { //indice(k) retourne la position de la case d'id "k"
+    for (i = 0; i < 9; i++) {
+        if (k == grille.children[i].id) {
+            return i;
+        }
+    }
+}
+
+function a_cote(i, j) {
+    if (i%3 === j%3 && Math.abs((i/3 |0) - (j/3 |0)) === 1) { //i%3 donne la ligne
+        return true;
+    } else return Math.abs(i - j) === 1 && (i / 3 | 0) === (j / 3 | 0);
+}
+
+function echange(i, j) {
+    var copie1 = grille.children[i].cloneNode([true]);
+    var copie2 = grille.children[j].cloneNode([true]);
+    grille.replaceChild(copie1, grille.children[j]);
+    grille.replaceChild(copie2, grille.children[i]);
+}
+
+function bouge(k) {
+    var i = indice(k);
+    var j = indice(8);
+    if (a_cote(i,j)) {
+        echange(i,j);
+        victoire();
+    }
 }
